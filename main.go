09aaae2423f9f09b11b9cebb1bd49b44b9d7bb5f
Victor Payno <vpayno@users.noreply.github.com>
2024-03-05 15:53:35 -0800

    chore: add basic hello world go program with tests

---
+++ main.go
@@ -0,0 +1,13 @@
+// This is a simple Hello World program.
+// The tests are fancier than the program.
+package main
+
+import "fmt"
+
+func helloWorld() {
+	fmt.Println("Hello world!")
+}
+
+func main() {
+	helloWorld()
+}
